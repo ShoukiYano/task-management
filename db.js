2b3be07311f570080331a9ecf2be6f373684378b
yanoshoki <yanopitrombone@gmail.com>
2025-02-13 11:52:46 +0900

    task-management-system ver.1.0.9

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
+// db.js
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";  // これを追加
+
 const { Pool } = require('pg');
 
-// 接続文字列を直接コード内に記述（例：Railway の PostgreSQL 接続情報）
-const connectionString = "postgresql://postgres:XmuQMfyOkrrugmLpWFweqzidUqlozhsq@postgres.railway.internal:5432/railway?sslmode=require";
-
 const pool = new Pool({
-  connectionString: connectionString,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
